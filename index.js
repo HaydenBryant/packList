c44fb3575ca620529763f5fcbfae3807c9669f18
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-04-12 16:39:32 +0000

    class change on check

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 $(document).ready(function() {
 
-    $('.add').click(function(){
+    //Add item to packlist
+    $('.add').click(function() {
         var item = $('.addItem').val();
-        if (item.length > 0){
-            $('#pack-list ul').append("<li>\n<input class='unpacked' type='checkbox' />\n<span class='name'>" + item + "</span>\n<span class='delete'>delete</span>\n</li>\n");
+        if (item.length > 0) {
+            $('#pack-list ul').append("<li>\n<input id='itmCheck' class='unpacked' type='checkbox' />\n<span class='name'>" + item + "</span>\n<span class='delete'>delete</span>\n</li>\n");
             $('.addItem').val("");
-        } else {
+        }
+        else {
             alert("Please enter an item!")
         }
     });
-    
-    $('#hide').click(function(){
-       $("#pack-list").slideToggle(); 
+
+    //slide up item list
+    $('#hide').click(function() {
+        $("#pack-list ul").slideToggle();
     });
-    
-    $('#pack-list').on('click', '.delete', function(){
+
+    //delete items on list
+    $('#pack-list').on('click', '.delete', function() {
         $(this.parentElement).remove();
     });
+
+    //Change checked item class
+    $('#pack-list').on('click', '#itmCheck', function() {
+        $(this).toggleClass('unpacked packed');
+    });
+
 });
